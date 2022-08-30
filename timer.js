baf6b410a1951eedaa62c981f434cd1accd8c122
Mac <mac@Macs-MacBook-Pro.local>
2022-08-30 16:08:38 +0400

    complete js countdown function

---
+++ timer.js
@@ -0,0 +1,63 @@
+
+   
+   const dayOutput = document.querySelector(".pDays")
+   const hourOutput = document.querySelector(".pHour")
+   const minuteOutput = document.querySelector(".pMinutes")
+   const secondOutput = document.querySelector(".pSeconds")
+
+const countDown = () =>{
+
+    const endDate =  new Date("Sep 10, 2022 13:20:00").getTime();
+    const now =  new Date().getTime();
+    
+    const difference = endDate - now;
+   
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+   
+
+
+    let timeDay = Math.floor(difference / day);
+    
+    let timeHour = Math.floor((difference % day) / hour);
+    let timeMinute = Math.floor((difference % hour) / minute);
+    let timeSecond = Math.floor((difference % minute) / second);
+
+
+ 
+   if(timeHour  < 10  ){
+     timeHour = "0" + timeHour 
+
+
+   }
+
+   if(timeMinute  < 10  ){
+
+    timeMinute = "0" + timeMinute
+
+
+  }
+  if(timeSecond  < 10  ){
+
+    timeSecond = "0" + timeSecond 
+
+
+  }
+
+
+    dayOutput.innerHTML=timeDay;
+    hourOutput.innerHTML = timeHour;
+    minuteOutput.innerHTML = timeMinute;
+    secondOutput.innerHTML = timeSecond;
+
+
+
+ 
+
+}
+
+setInterval(countDown, 1000);
+
